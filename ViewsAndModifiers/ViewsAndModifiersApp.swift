e2f21407d3ec87db718c7fc146e05db231355483
Stefan Olarescu <stefanolarescu@gmail.com>
2024-09-20 20:53:34 +0300

    Created Xcode project.

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Stefan Olarescu on 20.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
